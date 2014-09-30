10f75472e43cf09695895a3d4d67730880263c65
nykowicz <nicolas.sychowicz@gmail.com>
2014-09-30 20:56:03 +0200

    Vos gueules init

--- js/main.js
+++ js/main.js
@@ -18,6 +18,7 @@ $(document).ready(function(){
   	createjs.Sound.registerSound("./sound/barry.mp3", "sound",1);
   	createjs.Sound.registerSound("./sound/willy.mp3", "sound",1);
   	createjs.Sound.registerSound("./sound/chope.mp3", "sound",1);
+  	createjs.Sound.registerSound("./sound/vos-gueules.mp3", "sound",1);
   		
 	var son = "son"
 	var echo = 0;
